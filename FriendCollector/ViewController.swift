d1c63fb654cfd2cb5889d052b5acf54a650a02b5
Joey Newfield <joey.newfield@gmail.com>
2018-01-09 14:40:03 -0800

    Initial Commit

---
+++ FriendCollector/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FriendCollector
+//
+//  Created by Joey Newfield on 1/9/18.
+//  Copyright © 2018 Joey Newfield. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
